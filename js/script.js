bdfd907fa41c2b70d11ee398c1201498baea22e7
Matteo <grecomatteo86@gmail.com>
2021-09-07 14:40:35 +0200

    second axios first test

--- js/script.js
+++ js/script.js
@@ -2,17 +2,32 @@ var app = new Vue (
     {
         el:'#app',
         data:{
-            baseUrl:'http://api.openweathermap.org/data/2.5/weather?q=',
-            cityName:'London,uk',
+            baseUrl:'http://api.openweathermap.org/data/2.5/',
+
+            currentState:'weather?q=',
+            minuteGraduality:'onecall?',
+
+            cityNameLon:'London,uk',
+            latLonCityLon:'lat=51.5085&lon=-0.1257',
+
             apiKey:'&APPID=934ece2c9c1a966bbd0ca6a0e103059b',
+
             celsiusDegrees:'&units=metric',
-            firstView: null
+
+            firstView: null,
+            secondView: null
         },
         mounted:function(){
             // prima chiamata firstView
-            axios.get(this.baseUrl + this.cityName + this.apiKey + this.celsiusDegrees)
+            axios.get(this.baseUrl + this.currentState + this.cityNameLon + this.apiKey + this.celsiusDegrees)
             .then((response) => {
                 this.firstView = response.data.name + ' ' + response.data.main.temp;
+            });
+            // seconda chiamata secondView
+            axios.get(this.baseUrl + this.minuteGraduality + this.latLonCityLon + this.celsiusDegrees + '&exclude=daily' + this.apiKey)
+            .then((response) => {
+                console.log(response.data.hourly);
+                //this.secondView = response.data.hourly.temp;
             })
         }
     }
